ac0468f803eb5531f3633c10dca45fca59de9f8b
emrebaspinar1605 <turkuz-elhamdulillah@hotmail.com>
2022-07-07 14:10:24 +0300

    sss

---
+++ Program.cs
@@ -0,0 +1,5 @@
+﻿// See https://aka.ms/new-console-template for more information
+int time =4;
+string sonuc=time<=18 ? "İyi günler": "İyi Geceler";
+sonuc=time>=6 &&time<=11 ?"Günaydın!":time>=12 && time<=18 ? "İyi Günler!":"İyi Geceler!";
+System.Console.WriteLine(sonuc);
